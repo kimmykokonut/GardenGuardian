afd0d254f8aecf88531b951493bf642552dcdf04
Kim Robinson <kimmykokonut@gmail.com>
2024-02-26 14:34:34 -0800

    add Post and Put request for seed

--- GardenAPI.Solution/GardenApi/Controllers/SeedsController.cs
+++ GardenAPI.Solution/GardenApi/Controllers/SeedsController.cs
@@ -29,6 +29,43 @@ public class SeedsController : ControllerBase
     }
     return foundSeed;
   }
+  [HttpPost]
+  public async Task<ActionResult<Seed>> Post(Seed seed)
+  {
+    _db.Seeds.Add(seed);
+    await _db.SaveChangesAsync();
+    return CreatedAtAction(nameof(GetSeed), new { id = seed.SeedId }, seed);
+  }
+  [HttpPut("{id}")]
+  public async Task<IActionResult> Put(int id, Seed seed)
+  {
+    if (id != seed.SeedId)
+    {
+      return BadRequest();
+    }
+    _db.Seeds.Update(seed);
+
+    try 
+    {
+      await _db.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+      if (!SeedExists(id))
+      {
+        return NotFound();
+      }
+      else
+      {
+        throw;
+      }
+    }
+    return NoContent();
+  }
+  private bool SeedExists(int id)
+  {
+    return _db.Seeds.Any(e => e.SeedId == id);
+  }
 
 
 }
